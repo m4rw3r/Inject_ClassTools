b9a8d915d532ff60d2c703278ee6370bad20df41
m4rw3r <m4rw3r@gmail.com>
2011-10-20 17:05:11 +0200

    Phix Component Skeleton: Updated to version 8

--- src/tests/unit-tests/bootstrap.php
+++ src/tests/unit-tests/bootstrap.php
@@ -1,18 +1,5 @@
 <?php
 
-// =========================================================================
-//
-// tests/bootstrap.php
-//		A helping hand for running our unit tests
-//
-// Author	Stuart Herbert
-//		(stuart.herbert@gradwell.com)
-//
-// Copyright	(c) 2010 Gradwell dot com Ltd
-//		All rights reserved
-//
-// =========================================================================
-
 $paths = array(realpath(__DIR__ . '/../../php'), realpath(__DIR__ . '/../../../vendor/php'), realpath(__DIR__ . '/php'));
 $paths = array_merge($paths, array_map(function($path)
 {
@@ -21,6 +8,8 @@ $paths = array_merge($paths, array_map(function($path)
 
 spl_autoload_register(function($class) use($paths)
 {
+	$class = ltrim($class, '\\');
+	
 	if($index = strrpos($class, '\\'))
 	{
 		$path = DIRECTORY_SEPARATOR.strtr(substr($class, 0, $index), '\\', DIRECTORY_SEPARATOR)
@@ -43,20 +32,3 @@ spl_autoload_register(function($class) use($paths)
 	
 	return false;
 });
-/*
-// step 1: create the APP_TOPDIR constant that all MF components require
-define('APP_TOPDIR', realpath(__DIR__ . '/../../php'));
-define('APP_LIBDIR', realpath(__DIR__ . '/../../../vendor/php'));
-define('APP_TESTDIR', realpath(__DIR__ . '/php'));
-
-// step 2: add the lib-vendor code to the include path
-set_include_path(APP_LIBDIR . PATH_SEPARATOR . get_include_path());
-
-// step 3: add the tests folder to the include path
-set_include_path(APP_TESTDIR . PATH_SEPARATOR . get_include_path());
-
-// step 4: add our code to the include path
-set_include_path(APP_TOPDIR . PATH_SEPARATOR . get_include_path());
-
-// step 5: find the autoloader, and install it
-require_once(APP_LIBDIR . '/gwc.autoloader.php');
